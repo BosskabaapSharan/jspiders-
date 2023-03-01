f3e14d7a864a910b57427a91f6cfa30a317e09c1
BosskabaapSharan <Sharanprasad1@gmail.com>
2023-03-01 15:00:13 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+     <div>App</div>
+   <h1>sharan</h1>
+   <h6>2+2</h6>
+   <h6>num is {6+6}</h6>
+    </>
+   
   )
 }
 
