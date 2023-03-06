e9a12e597d3d204885696bb6ef4a5285d52e2a88
BosskabaapSharan <Sharanprasad1@gmail.com>
2023-03-06 19:22:41 +0530

    netflix done

---
+++ src/Netflix.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import STYLE from "./Netflix.module.css"
+
+
+const Netflix = () => {
+   return (
+      <div className={STYLE.aa}>
+
+        
+     
+
+     <div>
+    <img src="https://www.bing.com/th?id=OIP.DkCLrB5suQuau6Jl_jaiogHaB_&w=350&h=94&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" alt="" />
+    </div>
+ 
+  
+
+
+
+    <div className={STYLE.dd}>
+    <ul >
+
+        <li>
+            <select name="b" id="b">
+            <option value="eng">ENGLISH</option>
+            <option value="hi">HINDI</option>
+            </select>
+        </li>
+
+        <li>
+            <button type='submit' bgcolor="2px solid red">SIGN IN</button>
+        </li>
+    
+    </ul>
+         </div>
+    
+    
+    </div>
+   )
+}
+
+export default Netflix
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import {createRoot} from "react-dom/client"
 
 
-import Nav5 from './Nav5';
 
-createRoot(document.getElementById("root")).render(<Nav5/>)
+import Netflix from './Netflix';
+
+createRoot(document.getElementById("root")).render(<Netflix/>)
