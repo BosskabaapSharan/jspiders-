38ea78f68e130f717a02f8a80a345be955c68cf7
BosskabaapSharan <Sharanprasad1@gmail.com>
2023-03-10 00:08:27 +0530

    done

--- src/Details.jsx
+++ src/Details.jsx
@@ -17,7 +17,93 @@ const Details = (x) => {
             <h4>{x.d[1].yearofPass}</h4>
             <h4>{x.d[1].branch}</h4>
         </div>
-        
+        <div className='b'>
+        <h4>{x.d[2].studentName}</h4>
+            <h4>{x.d[2].studentId}</h4>
+            <h4>{x.d[2].yearofPass}</h4>
+            <h4>{x.d[2].branch}</h4>
+        </div>
+        <div className='b'>
+        <h4>{x.d[3].studentName}</h4>
+            <h4>{x.d[3].studentId}</h4>
+            <h4>{x.d[3].yearofPass}</h4>
+            <h4>{x.d[3].branch}</h4>
+        </div>
+        <div className='b'>
+        <h4>{x.d[4].studentName}</h4>
+            <h4>{x.d[4].studentId}</h4>
+            <h4>{x.d[4].yearofPass}</h4>
+            <h4>{x.d[4].branch}</h4>
+        </div>
+        <div className='b'>
+        <h4>{x.d[5].studentName}</h4>
+            <h4>{x.d[5].studentId}</h4>
+            <h4>{x.d[5].yearofPass}</h4>
+            <h4>{x.d[5].branch}</h4>
+        </div>
+        <div className='b'>
+        <h4>{x.d[6].studentName}</h4>
+            <h4>{x.d[6].studentId}</h4>
+            <h4>{x.d[6].yearofPass}</h4>
+            <h4>{x.d[6].branch}</h4>
+        </div>
+        <div className='b'>
+        <h4>{x.d[7].studentName}</h4>
+            <h4>{x.d[7].studentId}</h4>
+            <h4>{x.d[7].yearofPass}</h4>
+            <h4>{x.d[7].branch}</h4>
+        </div>
+        <div className='b'>
+        <h4>{x.d[8].studentName}</h4>
+            <h4>{x.d[8].studentId}</h4>
+            <h4>{x.d[8].yearofPass}</h4>
+            <h4>{x.d[8].branch}</h4>
+        </div>
+        <div className='b'>
+        <h4>{x.d[9].studentName}</h4>
+            <h4>{x.d[9].studentId}</h4>
+            <h4>{x.d[9].yearofPass}</h4>
+            <h4>{x.d[9].branch}</h4>
+        </div>
+        <div className='b'>
+        <h4>{x.d[10].studentName}</h4>
+            <h4>{x.d[10].studentId}</h4>
+            <h4>{x.d[10].yearofPass}</h4>
+            <h4>{x.d[10].branch}</h4>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
   )
 }
